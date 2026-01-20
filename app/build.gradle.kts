84fe9a77f564dc21e4c5ffaf479260c1400fdd2b
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-20 22:46:53 +0530

    fix deprecated kotlinOptions for kotlin 2.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,12 +64,15 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
     buildFeatures {
         compose = true
         buildConfig = true
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
     }
 }
 
