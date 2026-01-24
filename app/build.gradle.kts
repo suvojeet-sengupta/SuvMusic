ffd8cda5edfd2a40265369c2d533f8d528ad47fd
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-24 20:31:10 +0530

    Bump version to 1.0.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.suvojeet.suvmusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
