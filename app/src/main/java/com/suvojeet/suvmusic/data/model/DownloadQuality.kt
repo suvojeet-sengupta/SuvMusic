d8e2da9cf0144635b8ce959458784c1c3982f62f
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-01-24 13:58:34 +0530

    limit streaming and download quality to high (256kbps for yt, 320kbps for saavn) and remove very high option

--- app/src/main/java/com/suvojeet/suvmusic/data/model/DownloadQuality.kt
+++ app/src/main/java/com/suvojeet/suvmusic/data/model/DownloadQuality.kt
@@ -7,6 +7,5 @@ package com.suvojeet.suvmusic.data.model
 enum class DownloadQuality(val label: String, val maxBitrate: Int) {
     LOW("Low (64 kbps) • Saves data", 64),
     MEDIUM("Medium (128 kbps)", 128),
-    HIGH("High (256 kbps)", 256),
-    BEST("Best available", Int.MAX_VALUE);
+    HIGH("High (256 kbps)", 256);
 }
