89f20ef23c2a954220adf40289c39fce0e1c9bc4
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-02-27 23:14:20 +0530

    chore: setup splash screen theme and update version to 1.3.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 12
-        versionName = "1.3.1.0"
+        versionName = "1.3.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
