01252d3e8f05609ce89c56f7b7f5a903685a03b7
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-03-09 14:04:10 +0530

    feat: refactor miniplayer and add YT Music style

--- app/src/main/java/com/suvojeet/suvmusic/data/model/MiniPlayerStyle.kt
+++ app/src/main/java/com/suvojeet/suvmusic/data/model/MiniPlayerStyle.kt
@@ -2,5 +2,6 @@ package com.suvojeet.suvmusic.data.model
 
 enum class MiniPlayerStyle(val label: String) {
     STANDARD("Standard"),
-    FLOATING_PILL("Floating Pill")
+    FLOATING_PILL("Floating Pill"),
+    YT_MUSIC("YouTube Music")
 }
