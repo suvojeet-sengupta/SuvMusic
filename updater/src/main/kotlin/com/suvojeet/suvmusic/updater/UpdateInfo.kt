859af6752e0c67a43812a43e80361e97cd2f5654
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-03-09 11:07:53 +0530

    chore: remove redundant changelog from update.json and update UI to handle it

--- updater/src/main/kotlin/com/suvojeet/suvmusic/updater/UpdateInfo.kt
+++ updater/src/main/kotlin/com/suvojeet/suvmusic/updater/UpdateInfo.kt
@@ -6,7 +6,7 @@ import kotlinx.serialization.Serializable
 data class UpdateInfo(
     val versionName: String,
     val versionCode: Int,
-    val changelog: String,
+    val changelog: String = "",
     val downloadUrl: String,
     val forceUpdate: Boolean
 )
