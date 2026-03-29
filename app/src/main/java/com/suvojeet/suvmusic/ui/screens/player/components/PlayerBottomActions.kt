7c5bd0cb5f3f438e22beaded607c0ae7ad33a95d
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-03-29 17:42:15 +0530

    feat: implement Related songs logic in player screen

--- app/src/main/java/com/suvojeet/suvmusic/ui/screens/player/components/PlayerBottomActions.kt
+++ app/src/main/java/com/suvojeet/suvmusic/ui/screens/player/components/PlayerBottomActions.kt
@@ -48,6 +48,7 @@ fun BottomActions(
     onLyricsClick: () -> Unit,
     onCastClick: () -> Unit,
     onQueueClick: () -> Unit,
+    onRelatedClick: () -> Unit,
     onDownloadClick: () -> Unit,
     downloadState: com.suvojeet.suvmusic.data.model.DownloadState,
     dominantColors: DominantColors,
@@ -77,9 +78,9 @@ fun BottomActions(
 
         BottomTabButton(
             label = "RELATED",
-            onClick = { /* Not implemented yet */ },
+            onClick = onRelatedClick,
             dominantColors = dominantColors,
-            enabled = false
+            enabled = true
         )
     }
 }
