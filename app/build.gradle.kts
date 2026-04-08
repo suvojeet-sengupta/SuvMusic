94e47201f9f5a1fd2823ea9474f6c83ce14515d9
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-04-09 00:12:22 +0530

    chore: bump version to 2.2.2.0 and update changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.suvojeet.suvmusic"
         minSdk = 26
         targetSdk = 36
-        versionCode = 24
-        versionName = "2.2.1.0"
+        versionCode = 25
+        versionName = "2.2.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
