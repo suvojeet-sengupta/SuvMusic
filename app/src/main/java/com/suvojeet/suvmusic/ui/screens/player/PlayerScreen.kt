8cfe4e9c1a5cea0df09ba90c5430fca7b80889b2
suvojeet-sengupta <suvojitsengupta21@gmail.com>
2026-06-21 14:26:10 +0200

    Fix compilation errors in PlayerScreen due to missing imports

--- app/src/main/java/com/suvojeet/suvmusic/ui/screens/player/PlayerScreen.kt
+++ app/src/main/java/com/suvojeet/suvmusic/ui/screens/player/PlayerScreen.kt
@@ -11,6 +11,9 @@ import androidx.compose.animation.fadeOut
 import androidx.compose.animation.slideInVertically
 import androidx.compose.animation.slideOutVertically
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
